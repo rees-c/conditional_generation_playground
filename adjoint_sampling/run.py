b44e1a678ecbf6c8b5c48adbdc0f0cec4df03739
Rees Chang <rees.chang@gmail.com>
2025-06-10 13:49:10 -0500

    sparser reward

--- adjoint_sampling/run.py
+++ adjoint_sampling/run.py
@@ -9,19 +9,20 @@ from adjoint_sampling.model import DiffusionModel
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    energy_fn: nn.Module = get_energy_function(device)
 
     # Hyperparameters
     lr = 1e-3
-    num_epochs = 201
+    num_epochs = 301
     num_batches_per_epoch = 100
-    batch_size = 64
-    buffer_size = 1_000
+    batch_size = 96
+    buffer_size = 5_000
     n_log_prints = 10
     log_freq = int(num_epochs / n_log_prints)
-    energy_temperature = 1.0  #0.01
+    energy_temperature = 0.07  #0.01
     max_target_score_norm = 20.0
+    sparse_reward = True
 
+    energy_fn: nn.Module = get_energy_function(device, sparse=sparse_reward)
     model = DiffusionModel(sigma_min=0.01, sigma_max=0.4)
     model.to(device)
     buffer = ReplayBuffer(int(buffer_size / batch_size))
@@ -32,14 +33,13 @@ def main():
     grid_x, grid_y = torch.meshgrid(torch.linspace(-0.5, 0.5, steps=25), torch.linspace(-0.5, 0.5, steps=25))
     grid = torch.stack([grid_x.reshape(-1), grid_y.reshape(-1)], dim=-1).to(device)
 
-    target_dist = torch.exp(-energy_fn(grid) / energy_temperature) #energy_fn.gmm.log_prob(grid).exp()
+    target_dist = torch.exp(-energy_fn(grid)) #energy_fn.gmm.log_prob(grid).exp()
     plt.contourf(grid_x.cpu(), grid_y.cpu(), target_dist.view(25, 25).cpu())
     samples = model.sample(100).clamp(min=-0.5, max=0.5)
     samples = samples.cpu().numpy()
     plt.plot(samples[:, 0], samples[:, 1], "rx", alpha=0.5)
-    plt.show()
-    # plt.savefig("adjoint_sampling_epoch0.png")
-    # plt.close()
+    plt.savefig(f"adjoint_sampling_{'sparse' if sparse_reward else ''}_epoch0.png")
+    plt.close()
 
     for epoch in range(num_epochs):
         # Sample from model and store in buffer
@@ -71,13 +71,13 @@ def main():
     # Plot result
     grid_x, grid_y = torch.meshgrid(torch.linspace(-0.5, 0.5, steps=25), torch.linspace(-0.5, 0.5, steps=25))
     grid = torch.stack([grid_x.reshape(-1), grid_y.reshape(-1)], dim=-1).to(device)
-    target_dist = torch.exp(-energy_fn(grid) / energy_temperature) #energy_fn.gmm.log_prob(grid).exp()
+    target_dist = torch.exp(-energy_fn(grid)) #energy_fn.gmm.log_prob(grid).exp()
     plt.contourf(grid_x.cpu(), grid_y.cpu(), target_dist.view(25, 25).cpu())
 
     samples = model.sample(100).clamp(min=-0.5, max=0.5)
     samples = samples.cpu().numpy()
     plt.plot(samples[:, 0], samples[:, 1], "rx", alpha=0.5)
-    plt.savefig(f"adjoint_sampling_epoch{epoch}.png")
+    plt.savefig(f"adjoint_sampling_{'sparse' if sparse_reward else ''}_epoch{epoch}.png")
     plt.close()
 
 
--- rewards.py
+++ rewards.py
@@ -8,19 +8,27 @@ import matplotlib.pyplot as plt
 from aliases import *
 
 
-def get_energy_function(device: torch.device) -> nn.Module:
-    n_components = 3
-    loc = torch.stack(
-      [
-          torch.linspace(-0.21, 0.2, steps=n_components, device=device),
-          torch.linspace(-0.21, 0.2, steps=n_components, device=device),
-      ], dim=-1
-    )
-    scale = 0.3 * torch.tensor(
-        [[0.5028, 0.4018],
-          [0.2751, 0.6016],
-          [0.3351, 0.4889]]
-    )
+def get_energy_function(device: torch.device, sparse: bool = False) -> nn.Module:
+    if sparse:
+        loc = torch.cartesian_prod(
+            torch.tensor([-0.3, 0.0, 0.3], device=device),
+            torch.tensor([-0.3, 0.0, 0.3], device=device)
+        )
+        scale = 0.07 * torch.rand_like(loc) + 0.05
+        n_components = scale.shape[0]
+    else:
+        n_components = 3
+        loc = torch.stack(
+          [
+              torch.linspace(-0.21, 0.2, steps=n_components, device=device),
+              torch.linspace(-0.21, 0.2, steps=n_components, device=device),
+          ], dim=-1
+        )
+        scale = 0.3 * torch.tensor(
+            [[0.5028, 0.4018],
+              [0.2751, 0.6016],
+              [0.3351, 0.4889]]
+        )
     components = Independent(Normal(loc, scale), 1)  # [batch_shape: (5, 2), event_shape: (,)] -> [batch_shape: (5,), event_shape: (2,)]
     mixture_probs = Categorical(torch.ones(n_components, device=device))
     gmm = MixtureSameFamily(mixture_probs, components)
@@ -45,6 +53,35 @@ class EnergyModel(nn.Module):
             grad = torch.autograd.grad(self(x).sum(), x)[0]
             x.requires_grad_(False)
         return grad
+
+
+class DiscreteEnergyModel(nn.Module):
+    """Discretize a gmm"""
+    def __init__(self, gmm: MixtureSameFamily):
+        super().__init__()
+        self.gmm = gmm
+
+        grid = torch.cartesian_prod(
+            torch.linspace(-0.5, 0.5, steps=10),
+            torch.linspace(-0.5, 0.5, steps=10)
+        )  # (100, 2)
+        energies = -gmm.log_prob(grid).reshape(-1)
+        self.register_buffer("energies", energies)
+
+    def forward(self, indices: Tensor) -> Tensor:
+        """
+        Args:
+            indices: torch.long
+                shape (batch_size,)
+
+        Returns:
+            shape (batch_size,)
+        """
+        assert (
+            indices.dtype == torch.long
+            and 0 <= indices <= self.energies.numel() - 1
+        )
+        return self.energies[indices]
 
 
 @torch.no_grad()
@@ -78,4 +115,4 @@ def plot_continuous_reward(device):
 
 
 if __name__ == "__main__":
-    plot_continuous_reward()
+    plot_continuous_reward("cpu")
